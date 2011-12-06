d6f5305b3567c2a8a930f9b9ccfb051f56eafcc9
Tanner Smith <tanner@tssoftware.net>
2011-12-05 20:56:34 -0500

    Test AND

--- test.asm
+++ test.asm
@@ -1,19 +1,12 @@
     .ORIG x3000
 
-    AND R1, R1, 0
-    ADD R1, R1, 5
-    
     AND R0, R0, 0
-ME  ADD R0, R0, 15
-
-    LEA R3, STRING
-    LDR R2, R3, 0
-
+    AND R1, R1, 0
+    AND R2, R2, 0
+    AND R3, R3, 0
     AND R4, R4, 0
-    ADD R4, R4, 0xFFFF
-    NOT R4, R4
+    AND R5, R5, 0
+    AND R6, R6, 0
+    AND R7, R7, 0
 
     HALT
-
-STRING .fill 0x2419
-    .fill 0xFF00
