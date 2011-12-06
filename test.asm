234c9934d16160da9f41938f3fb0f4bc3a4d4021
Tanner Smith <tanner@tssoftware.net>
2011-12-05 20:43:29 -0500

    Update test fiel

--- test.asm
+++ test.asm
@@ -1,8 +1,19 @@
     .ORIG x3000
 
+    AND R1, R1, 0
+    ADD R1, R1, 5
+    
     AND R0, R0, 0
 ME  ADD R0, R0, 15
 
-    LDR R1, R6, 0
+    LEA R3, STRING
+    LDR R2, R3, 0
+
+    AND R4, R4, 0
+    ADD R4, R4, 0xFFFF
+    NOT R4, R4
 
     HALT
+
+STRING .fill 0x2419
+    .fill 0xFF00
